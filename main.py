b0d9c1dab6c712d19f50a2ed33de0489c6dc1e39
Jake Robson <jrobsonsws@gmail.com>
2020-03-23 19:56:33 +0000

    Tested how the API works. A lot of commented out code which will be removed.

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import requests
 import json
+from datetime import date, timedelta
 
 API_KEY = 'ENTER_API_KEY_HERE'
-API_URL = f'https://api.pandascore.co/csgo/matches?range[begin_at]=2020-03-22T00:00:00Z,2020-03-22T23:59:59&token={API_KEY}'
+TEAMS_API_URL = f'https://api.pandascore.co/csgo/teams?filter[id]=3209&token={API_KEY}'
 
 def getData():
-    response = requests.get(API_URL)
-    response = json.dumps(response.json(), indent=4, sort_keys=True)
+    today = date.today().strftime('%Y-%m-%d')
+    startDate = f'{today}T00:00:00Z'
+    endDate = f'{today}T23:59:59Z'
+    MATCHES_API_URL = f'https://api.pandascore.co/csgo/matches?range[begin_at]={startDate},{endDate}&per_page=100&sort=begin_at&token={API_KEY}'
+    response = requests.get(MATCHES_API_URL)
+    #print(f'Pages Total: {response.headers["X-Total"]}')
+    response = response.json()
+    #print(f'Matches: {len(response)}')
+    for i in range(len(response)):
+        print(response[i]["name"])
+        i += 1
+    # response = json.dumps(response, indent=4, sort_keys=True)
+    # with open('response.json', 'w+') as f:
+    #     f.write(response)
+
+def getTeams():
+    response = requests.get(TEAMS_API_URL)
+    # pages = response.headers['X-Total']
+    # print(pages)
+    response = response.json()
     print(response)
+    # for i in range(len(response)):
+    #     team = response[i]['name']
+    #     print(team)
+    #     i+=1
 
 def main():
     getData()
